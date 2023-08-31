4c7b5075a077e4cf625081bd4c19692d12e8d130
vm-packages <vm-packages@mandiant.com>
2023-08-31 00:47:12 +0000

    Update upx.vm to 4.1.0

--- packages/upx.vm/tools/chocolateyinstall.ps1
+++ packages/upx.vm/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ Import-Module vm.common -Force -DisableNameChecking
 $toolName = 'upx'
 $category = 'Utilities'
 
-$zipUrl = 'https://github.com/upx/upx/releases/download/v4.0.2/upx-4.0.2-win64.zip'
-$zipSha256 = '325c58ea2ed375afbd4eeac0b26f15f98db0d75dea701205ca10d8bf4d2fdc24'
+$zipUrl = 'https://github.com/upx/upx/releases/download/v4.1.0/upx-4.1.0-win64.zip'
+$zipSha256 = '382cee168d6261a76c3b6a98b3ca2de44930bf5faa5f2dc2ced4fa3850fe8ff6'
 
 VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256 -consoleApp $true -innerFolder $true
