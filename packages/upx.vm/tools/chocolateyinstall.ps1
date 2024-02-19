f38546aaaca16af731357590f1b94c0c1f8a521a
vm-packages <vm-packages@mandiant.com>
2024-02-19 13:33:47 +0000

    Add upx.vm
    Closes https://github.com/Ana06/VM-Packages/issues/1.

--- packages/upx.vm/tools/chocolateyinstall.ps1
+++ packages/upx.vm/tools/chocolateyinstall.ps1
@@ -2,11 +2,9 @@ $ErrorActionPreference = 'Stop'
 Import-Module vm.common -Force -DisableNameChecking
 
 $toolName = 'upx'
-$category = 'Packers'
+$category = 'Utilities'
 
-$zipUrl = "https://github.com/upx/upx/releases/download/v4.2.2/upx-4.2.2-win32.zip"
-$zipSha256 = "141e7cd8d009b827590662b482f1ae2f1dda17cf446a5651078235efb1429c59"
-$zipUrl_64 = 'https://github.com/upx/upx/releases/download/v4.2.2/upx-4.2.2-win64.zip'
-$zipSha256_64 = '7c69b92ff2b7fb8122609312eb0a6695c9e4b88a6c784a86c92e3b7ef583281f'
+$zipUrl = 'https://github.com/upx/upx/releases/download/v4.0.2/upx-4.0.2-win64.zip'
+$zipSha256 = '325c58ea2ed375afbd4eeac0b26f15f98db0d75dea701205ca10d8bf4d2fdc24'
 
-VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256 -zipUrl_64 $zipUrl_64 -zipSha256_64 $zipSha256_64 -consoleApp $true -innerFolder $true
+VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256 -consoleApp $true -innerFolder $false
